abe76397b70a9f89fc455ec42edeccfe59fd2ee3
Anton Larin <larinam@gmail.com>
2023-11-30 19:43:04 +0100

    database user initiation for docker-compose configuration

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ from bson import ObjectId
 from mongoengine import StringField, DateField, ListField, connect, Document, EmbeddedDocument, \
     EmbeddedDocumentListField, ObjectIdField
 
+from pymongo import MongoClient
+
 import dotenv
 import os
 import logging
@@ -19,9 +21,30 @@ mongo_port = os.getenv("MONGO_PORT")
 mongo_db_name = os.getenv("MONGO_DB_NAME")
 
 if mongo_username:  # connect to some external MongoDB
-    mongo_connection_string = f"mongodb://{mongo_username}:{mongo_password}@{mongo_host}:{mongo_port}/{mongo_db_name}"
+    if os.getenv("MONGO_INITDB_ROOT_USERNAME") and os.getenv("MONGO_INITDB_ROOT_PASSWORD"):
+        log.info("Initiating MongoDB user")
+        admin_db_uri = f"mongodb://{os.getenv("MONGO_INITDB_ROOT_USERNAME")}:{os.getenv("MONGO_INITDB_ROOT_PASSWORD")}@{mongo_host}:{mongo_port}/admin"
+        with (MongoClient(admin_db_uri) as client):
+            admin_db = client['admin']
+            existing_user = admin_db.system.users.find_one({"user": mongo_username})
+            if not existing_user:
+                user_command = {
+                    "createUser": mongo_username,
+                    "pwd": mongo_password,
+                    "roles": [
+                        {
+                            "role": "readWrite",
+                            "db": mongo_db_name
+                        }
+                    ]
+                }
+                admin_db.command(user_command)
+                log.info("MongoDB user created")
+            else:
+                log.info("MongoDB user already exists")
+    mongo_connection_string = f"mongodb://{mongo_username}:{mongo_password}@{mongo_host}:{mongo_port}/"
     log.info(f"MongoDB connection string: {mongo_connection_string}")
-    connect("vacal", host=mongo_connection_string)
+    connect(mongo_db_name, host=mongo_connection_string)
 else:  # just local MongoDB
     log.info("Connecting to local MongoDB")
     connect("vacal")
