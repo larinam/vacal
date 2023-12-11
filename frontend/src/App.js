28fbc6c600248a7bba24c18e092feb6aa2259e9d
mrgazanfarli <mr.gazanfarli@gmail.com>
2023-12-11 14:56:49 +0400

    Keep token in session storage and make minor improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainComponent from './components/MainComponent';
 import Login from './components/Login';
 import './styles.css';
+import { AUTH_TOKEN_KEY } from "./consts";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(!!sessionStorage.getItem(AUTH_TOKEN_KEY));
   const [authHeader, setAuthHeader] = useState('');
 
   const handleLogin = (username, password) => {
     const encodedCredentials = btoa(`${username}:${password}`);
-    setAuthHeader(`Basic ${encodedCredentials}`);
+    const token = `Basic ${encodedCredentials}`;
+    sessionStorage.setItem(AUTH_TOKEN_KEY, token)
+    setAuthHeader(token);
     setIsAuthenticated(true);
     // Additional logic for handling login can be added here
   };
+
+  useEffect(() => {
+    const token = sessionStorage.getItem(AUTH_TOKEN_KEY);
+
+    if (token) {
+      setAuthHeader(token);
+    } else {
+
+    }
+  }, []);
 
   // Check the updated environment variable
   const requiresBasicAuth = process.env.REACT_APP_REQUIRE_BASIC_AUTH === 'true';
