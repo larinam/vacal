cc84d03442fb0c1b31e3d6088389a5a448560434
Anton Larin <larinam@gmail.com>
2024-02-10 11:42:29 +0100

    hotkeys for month change

--- frontend/src/components/MonthSelector.jsx
+++ frontend/src/components/MonthSelector.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './MonthSelector.css';
 
 const MonthSelector = ({ displayMonth, setDisplayMonth, todayYear, todayMonth }) => {
     const changeMonth = (offset) => {
-        const newMonth = new Date(displayMonth.setMonth(displayMonth.getMonth() + offset));
+        const newMonth = new Date(displayMonth.getFullYear(), displayMonth.getMonth() + offset, 1);
         setDisplayMonth(newMonth);
     };
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            const focusWithinInteractive = ['INPUT', 'TEXTAREA', 'SELECT'].includes(document.activeElement.tagName) || document.activeElement.isContentEditable;
+
+            // If not within an input or textarea, or editable area then check for arrow keys
+            if (!focusWithinInteractive) {
+                if (event.key === 'ArrowLeft') {
+                    changeMonth(-1);
+                } else if (event.key === 'ArrowRight') {
+                    changeMonth(1);
+                }
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        // Cleanup to remove the event listener
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [displayMonth, setDisplayMonth]);
 
     return (
         <div className="monthSelector">
