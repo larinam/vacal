2eac4baea599aebe1d124ad3615ed6a9be880eac
Anton Larin <larinam@gmail.com>
2024-08-25 12:32:43 +0200

    fix settings nav items design and selection logic

--- frontend/src/components/settings/SettingsComponent.jsx
+++ frontend/src/components/settings/SettingsComponent.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
+import {NavLink, Route, Routes, useLocation} from 'react-router-dom';
 import DayTypes from './DayTypes';
 import UserManagement from './UserManagement';
-import {NavLink, Route, Routes} from 'react-router-dom';
 import './SettingsComponent.css';
 
-const SettingsComponent = ({ onClose }) => {
-    return (
-        <div className="settingsContainer">
-            <div className="settingsNavigation">
-                <button onClick={onClose} className="closeButton">Close</button>
-                <NavLink to="daytypes" className="navItem" activeclassname="active">
-                    Day Types
-                </NavLink>
-                <NavLink to="usermanagement" className="navItem" activeclassname="active">
-                    Users
-                </NavLink>
-            </div>
-            <div className="settingsContent">
-                <Routes>
-                    <Route path="daytypes" element={<DayTypes />} />
-                    <Route path="usermanagement" element={<UserManagement />} />
-                    {/* Redirect to default tab */}
-                    <Route path="/" element={<DayTypes />} />
-                </Routes>
-            </div>
-        </div>
-    );
+const SettingsComponent = ({onClose}) => {
+  const location = useLocation();
+  const normalizedPath = location.pathname.replace(/\/$/, ''); // Remove trailing slash if it exists
+  const isDefaultActive = normalizedPath === '/main/settings';
+
+  return (
+    <div className="settingsContainer">
+      <div className="settingsNavigation">
+        <button onClick={onClose} className="closeButton">Close</button>
+        <NavLink
+          to="daytypes"
+          className={`navItem ${isDefaultActive ? 'active' : ''}`}
+          activeclassname="active"
+        >
+          Day Types
+        </NavLink>
+        <NavLink
+          to="usermanagement"
+          className="navItem"
+          activeclassname="active"
+        >
+          Users
+        </NavLink>
+      </div>
+      <div className="settingsContent">
+        <Routes>
+          <Route path="daytypes" element={<DayTypes/>}/>
+          <Route path="usermanagement" element={<UserManagement/>}/>
+          {/* Redirect to default tab */}
+          <Route path="/" element={<DayTypes/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default SettingsComponent;
