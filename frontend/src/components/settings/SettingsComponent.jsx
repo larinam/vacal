68b4b89f33a5c95b09c280dab9f60b6f070999a2
Anton Larin <larinam@gmail.com>
2025-01-02 19:30:57 +0100

    intro Subscription component

--- frontend/src/components/settings/SettingsComponent.jsx
+++ frontend/src/components/settings/SettingsComponent.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {NavLink, Route, Routes, useLocation} from 'react-router-dom';
 import DayTypes from './DayTypes';
 import UserManagement from './UserManagement';
+import SubscriptionManagement from "./SubscriptionManagement";
 import './SettingsComponent.css';
+import {useConfig} from "../../contexts/ConfigContext";
 
 const SettingsComponent = ({onClose}) => {
   const location = useLocation();
   const normalizedPath = location.pathname.replace(/\/$/, ''); // Remove trailing slash if it exists
   const isDefaultActive = normalizedPath === '/main/settings';
+  const {isMultitenancyEnabled} = useConfig();
 
   return (
     <div className="settingsContainer">
@@ -27,11 +30,23 @@ const SettingsComponent = ({onClose}) => {
         >
           Users
         </NavLink>
+        {isMultitenancyEnabled && (
+          <NavLink
+            to="subscription"
+            className="navItem"
+            activeclassname="active"
+          >
+            Subscription
+          </NavLink>
+        )}
       </div>
       <div className="settingsContent">
         <Routes>
           <Route path="daytypes" element={<DayTypes/>}/>
           <Route path="usermanagement" element={<UserManagement/>}/>
+          {isMultitenancyEnabled && (
+            <Route path="subscription" element={<SubscriptionManagement/>}/>
+          )}
           {/* Redirect to default tab */}
           <Route path="/" element={<DayTypes/>}/>
         </Routes>
---
+++ frontend/src/components/settings/SubscriptionManagement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SubscriptionManagement = () => {
+
+  return (
+    <div>
+      <h2>Subscription Management Settings</h2>
+
+    </div>
+  )
+}
+
+export default SubscriptionManagement;
