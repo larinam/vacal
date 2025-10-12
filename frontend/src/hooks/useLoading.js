7f92c725026d6a4210cb9056fae19405b441af47
Anton Larin <larinam@gmail.com>
2025-10-12 15:39:28 +0200

    Stabilize API hook callbacks to prevent subscription refetch loop

--- frontend/src/hooks/useLoading.js
+++ frontend/src/hooks/useLoading.js
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import {useCallback, useState} from 'react';
 
 export const useLoading = () => {
     const [isLoading, setIsLoading] = useState(false);
 
-    const startLoading = () => {
+    const startLoading = useCallback(() => {
         return setTimeout(() => {
             setIsLoading(true);
         }, 100);
-    };
+    }, []);
 
-    const stopLoading = (timer) => {
+    const stopLoading = useCallback((timer) => {
         clearTimeout(timer);
         setIsLoading(false);
-    };
+    }, []);
 
     return [isLoading, startLoading, stopLoading];
 };
